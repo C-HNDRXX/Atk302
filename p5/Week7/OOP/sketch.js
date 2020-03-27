473942faec9ea5ef533f5201ab6dd59c3dfd0c8a
C-HNDRXX <54365406+C-HNDRXX@users.noreply.github.com>
2020-03-27 12:32:57 -0500

    bbv

---
+++ p5/Week7/OOP/sketch.js
@@ -0,0 +1,33 @@
+var myCar;
+
+function setup() {
+  createCanvas(800, 800);
+  myCar = new Car();
+}
+
+function draw() {
+  background('lightblue');
+
+  myCar.display();
+  myCar.drive();
+
+}
+
+// our Car class!!!!!
+function Car() {
+  // atributes
+  this.x = 100;
+
+  // methods
+  this.display = function() {
+    rect(this.x, 100, 100, 100);
+  }
+
+  this.drive = function() {
+    this.x = this.x + 5;
+    if (this.x > width) {
+      this.x = 0;
+    }
+
+
+  }
