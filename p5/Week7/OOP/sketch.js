0081817e3fe854e8b5bae4ac48ea95803ba4291c
C-HNDRXX <54365406+C-HNDRXX@users.noreply.github.com>
2020-03-27 13:58:32 -0500

    new

--- p5/Week7/OOP/sketch.js
+++ p5/Week7/OOP/sketch.js
@@ -23,25 +23,26 @@ function draw() {
 
 // our Car class!!!!!
 function Car() {
-  // atributesthis.pos = createVector(random(width), random(height));
-this.vel = createVector(random(-5, 5), random(-5, 5));
-this.r = random(255);
-this.g = random(255);
-this.b = random(255);
+  // atributes
+  this.pos = createVector(random(width), random(height));
+  this.vel = createVector(random(-5, 5), random(-5, 5));
+  this.r = random(255);
+  this.g = random(255);
+  this.b = random(255);
 
-// methods
-this.display = function() {
-  fill(this.r, this.g, this.b);
-  rect(this.pos.x, this.pos.y, 100, 100, 100);
+  // methods
+  this.display = function() {
+    fill(this.r, this.g, this.b);
+    rect(this.pos.x, this.pos.y, 100, 100, 100);
 
-}
+  }
 
-this.drive = function() {
-  this.pos.add(this.vel);
-  if (this.pos.x > width) this.pos.x = 0;
-  if (this.pos.x < 0) this.pos.x = width;
-  if (this.pos.y > height) this.pos.y = 0;
-  if (this.pos.y < 0) this.pos.y = height;
-}
+  this.drive = function() {
+    this.pos.add(this.vel);
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.y > height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = height;
+  }
 
 }
